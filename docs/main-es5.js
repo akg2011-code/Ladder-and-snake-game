540cf21ff6083a084318bb652abb2a846b520e13
akg2011-code <akg201194@gmai.com>
2021-04-30 01:51:21 +0200

    changeSrcImages2

--- docs/main-es5.js
+++ docs/main-es5.js
@@ -1,8 +1,13 @@
 (function() {
-    function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i];
+    function _defineProperties(target, props) {
+        for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
             descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor); } }
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+        }
+    }
 
     function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
